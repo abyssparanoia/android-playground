da09075629e08f6d67e798c487b079230bb48ca2
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-02-23 02:48:26 +0900

    feat: added display mesasge activity

---
+++ starter/app/src/main/java/com/example/starter/DisplayMessageActivity.kt
@@ -0,0 +1,19 @@
+package com.example.starter
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class DisplayMessageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_message)
+
+        val intent: Intent = getIntent()
+        val message: String = intent.getStringExtra(MainActivity().EXTRA_MESSAGE)
+        val textView: TextView = findViewById(R.id.textView)
+        textView.setText(message)
+    }
+}
