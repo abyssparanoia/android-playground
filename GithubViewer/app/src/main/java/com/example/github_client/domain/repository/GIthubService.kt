86abe0dae13986ab10890ede26098618b4b5ad97
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-02-25 00:16:42 +0900

    feat: added github service interface

--- GithubViewer/app/src/main/java/com/example/github_client/domain/repository/GIthubService.kt
+++ GithubViewer/app/src/main/java/com/example/github_client/domain/repository/GIthubService.kt
@@ -7,4 +7,10 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface GithubService {
+
+    @GET("/users/{user}/repos")
+    suspend fun listProject(@Path("user") user:String): Response<List<Project>>
+
+    @GET("/repos/{user}/{reponame}")
+    suspend fun getProject(@Path("user") user:String, @Path("reponame") projectName: String): Response<Project>
 }
