220336f6e1ad0a9f057a5e6819d907c664551ba0
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-02-25 00:13:37 +0900

    feat: added retrofit

---
+++ GithubViewer/app/src/main/java/com/example/github_client/domain/repository/GIthubService.kt
@@ -0,0 +1,10 @@
+package com.example.github_client.domain.repository
+
+import com.example.github_client.domain.model.Project
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface GithubService {
+}
